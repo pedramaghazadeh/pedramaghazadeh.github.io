08175f92580a65835a7b4e3a81ebed78cc47359c
Pedram Aghazadeh <47250634+pedramaghazadeh@users.noreply.github.com>
2022-02-16 17:05:01 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,27 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass('sticky');
         }
+        if(this.scrollY > 100){
+            $('.scroll-up-btn').addClass('show');
+        }else{
+            $('.scroll-up-btn').removeClass('show');
+        }
     });
+
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["Computer Scientist", "Data Scientist", "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // slide-up
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    });
+
     //toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
